82e453f74324362b101d4efd0e272e77704b4e34
Abdullah Dawwi <thabetabdullah73@gmail.com>
2025-08-03 01:03:19 +0300

    update test_vesrion_1

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,7 +1,10 @@
-import os
-import subprocess
+# tests/test_version.py
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
 
 def test_version_output():
-    main_path = os.path.join("src", "main.py")
-    result = subprocess.run(["python", main_path], capture_output=True, text=True)
-    assert "HiveBox App Version: 0.0.1" in result.stdout
+    response = client.get("/version")
+    assert response.status_code == 200
+    assert response.json() == {"version": "0.0.1"}
