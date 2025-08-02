33b090701d2102f605924e45c40b9b095c19859b
Abdullah Dawwi <thabetabdullah73@gmail.com>
2025-08-03 01:38:30 +0300

    update 2

--- src/endpoints/version.py
+++ src/endpoints/version.py
@@ -1,10 +1,10 @@
 APP_VERSION = "0.0.1"
 
+def get_version() -> str:
+    return APP_VERSION
 
 def print_version():
-
     print(f"HiveBox App Version: {APP_VERSION}")
 
 if __name__ == "__main__":
     print_version()
-
