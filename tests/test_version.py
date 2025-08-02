bcacea66c2f1c8fb8c878102bf673b3cf4e57441
Abdullah Dawwi <thabetabdullah73@gmail.com>
2025-08-03 00:45:29 +0300

    update test_vesrion

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,7 @@
+import os
 import subprocess
 
 def test_version_output():
-    result = subprocess.run(["python", "main.py"], capture_output=True, text=True)
+    main_path = os.path.join("src", "main.py")
+    result = subprocess.run(["python", main_path], capture_output=True, text=True)
     assert "HiveBox App Version: 0.0.1" in result.stdout
