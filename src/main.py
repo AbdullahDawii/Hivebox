7f592be611ffdc2f14d4852b5f5150c566a88ad3
Abdullah Dawwi <thabetabdullah73@gmail.com>
2025-08-13 00:55:03 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -91,4 +91,15 @@ async def get_boxes_temp(boxs, session) -> list:
                 temps.append(sensor["lastMeasurement"]["value"])
     return temps
 
+APP_VERSION = "0.0.1"
 
+from src.main import app
+
+def get_version() -> str:
+    return APP_VERSION
+
+def print_version():
+    print(f"HiveBox App Version: {APP_VERSION}")
+
+if __name__ == "__main__":
+    print_version()
