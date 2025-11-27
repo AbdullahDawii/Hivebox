97d0a8d1c5939b88d5b18243ae1cd21a33195726
AbdullahDawii <thabetabdullah73@gmail.com>
2025-11-27 21:01:12 +0200

    Initial commit of the project

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ app = Flask(__name__)
 __version__ = "0.0.1"
 
 # Connect to Redis/Valkey container
-REDIS_HOST = os.getenv("REDIS_HOST", "valkey-service.default.svc.cluster.local")      # For Kubernetes Deployment
-# REDIS_HOST = os.getenv("REDIS_HOST", "172.20.0.2")
+# REDIS_HOST = os.getenv("REDIS_HOST", "valkey-service.default.svc.cluster.local")      # For Kubernetes Deployment
+REDIS_HOST = os.getenv("REDIS_HOST", "172.17.0.2") #Container
 redis_client = redis.Redis(host=REDIS_HOST, port=6379)
 
 # Connect to MinIO Storage
-MINIO_HOST = os.getenv("MINIO_ENDPOINT", "minio-release-minio-service.default.svc.cluster.local:9000")    # For Kubernetes Deployment
-# MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "172.17.0.2")
+# MINIO_HOST = os.getenv("MINIO_ENDPOINT", "minio-release-minio-service.default.svc.cluster.local:9000")    # For Kubernetes Deployment
+MINIO_HOST = os.getenv("MINIO_ENDPOINT", "172.17.0.3:9000")
 # MINIO_PORT = int(os.getenv("MINIO_PORT", "9000"))
 # MINIO_HOST = f"{MINIO_ENDPOINT}:{MINIO_PORT}"
-MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
-MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minio_admin")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minio_strong_password")
 MINIO_BUCKET = os.getenv("MINIO_BUCKET", "hivebox-data")
 
 minio_client = Minio(
